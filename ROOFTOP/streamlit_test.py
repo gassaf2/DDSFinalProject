19bc16d3fc2ac5371498d868b13e36f111b06869
gassaf2 <gassaf2@gmail.com>
2023-11-30 06:31:23 +0400

    f

--- ROOFTOP/streamlit_test.py
+++ ROOFTOP/streamlit_test.py
@@ -4,3 +4,9 @@ import numpy as np
 
 
 st.title('Roof Top Detection')
+
+# Add a text input field
+user_input = st.text_input("Enter some text:", "Default text")
+
+# Display the entered text
+st.write("You entered:", user_input)
