db70eb0383edc4a2fe12fd2800c03776e0a94015
gassaf2 <gassaf2@gmail.com>
2023-11-30 06:33:28 +0400

    after creating a tab

--- ROOFTOP/streamlit_test.py
+++ ROOFTOP/streamlit_test.py
@@ -5,6 +5,23 @@ import numpy as np
 
 st.title('Roof Top Detection')
 
+
+# Create tabs using beta_columns
+tabs = st.beta_columns(3)
+
+# Add content to each tab
+with tabs[0]:
+    st.header("Tab 1")
+    st.write("This is the content of Tab 1")
+
+with tabs[1]:
+    st.header("Tab 2")
+    st.write("This is the content of Tab 2")
+
+with tabs[2]:
+    st.header("Tab 3")
+    st.write("This is the content of Tab 3")
+
 # Add a text input field
 user_input = st.text_input("Enter some text:", "Default text")
 
