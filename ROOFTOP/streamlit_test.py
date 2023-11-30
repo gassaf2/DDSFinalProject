8844b47a4fe57af91cdfdcfcff190d8418f4969c
gassaf2 <gassaf2@gmail.com>
2023-11-30 06:41:56 +0400

    s

--- ROOFTOP/streamlit_test.py
+++ ROOFTOP/streamlit_test.py
@@ -6,19 +6,13 @@ import numpy as np
 st.title('Roof Top Detection')
 
 
-# Create tabs
-tabs = ["Tab 1", "Tab 2", "Tab 3"]
-selected_tab = st.radio("Select a tab:", tabs)
 
-# Add content based on the selected tab
-if selected_tab == "Tab 1":
-    st.header("Tab 1")
-    st.write("This is the content of Tab 1")
 
-elif selected_tab == "Tab 2":
-    st.header("Tab 2")
-    st.write("This is the content of Tab 2")
+# Add a text input field
+user_input = st.text_input("Enter some text:", "Default text")
 
-elif selected_tab == "Tab 3":
-    st.header("Tab 3")
-    st.write("This is the content of Tab 3")
+# Display the entered text
+st.write("You entered:", user_input)
+
+
+our_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
