3aa9ff6490732e33b7bcac20faabba1601061508
gassaf2 <gassaf2@gmail.com>
2023-11-30 13:26:54 +0400

    d

--- ROOFTOP/streamlit_test.py
+++ ROOFTOP/streamlit_test.py
@@ -18,3 +18,16 @@ with st.sidebar:
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
