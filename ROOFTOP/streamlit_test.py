d6d0c5644627573e5e604400b0a6c205016a043f
gassaf2 <gassaf2@gmail.com>
2023-11-30 13:19:34 +0400

    f

--- ROOFTOP/streamlit_test.py
+++ ROOFTOP/streamlit_test.py
@@ -5,47 +5,12 @@ import numpy as np
 
 st.title('Roof Top Detection')
 
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
 
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
 
-
-# Add a text input field
-user_input = st.text_input("Enter some text:", "Default text")
-
-# Display the entered text
-st.write("You entered:", user_input)
-
-
-our_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-
-
-
-if st.checkbox('Parameters of model'):
-    st.subheader('Raw data')
-    st.write("test test test test test test \n test test test test test")
-    
-    
-    
-animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
-
-animal = st.text_input('Type an animal')
-
-if st.button('Check availability'):
-    have_it = animal.lower() in animal_shelter
-    'We have that animal!' if have_it else 'We don\'t have that animal.'    
-    
-    
-# stateful button     
-if 'clicked' not in st.session_state:
-    st.session_state.clicked = False
-
-def click_button():
-    st.session_state.clicked = True
-
-st.button('Click me', on_click=click_button)
-
-if st.session_state.clicked:
-    # The message and nested widget will remain on the page
-    st.write('Button clicked!')
-    st.slider('Select a value')
-   
 
