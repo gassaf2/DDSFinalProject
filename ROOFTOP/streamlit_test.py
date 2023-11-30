f0aefbf5c9326d5a718e5c18f92a811f9756a842
gassaf2 <gassaf2@gmail.com>
2023-11-30 13:22:38 +0400

    tr

--- ROOFTOP/streamlit_test.py
+++ ROOFTOP/streamlit_test.py
@@ -5,12 +5,16 @@ import numpy as np
 
 st.title('Roof Top Detection')
 
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
 with st.sidebar:
-    with st.echo():
-        st.write("This code will be printed to the sidebar.")
-
-    with st.spinner("Loading..."):
-        time.sleep(5)
-    st.success("Done!")
-
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
