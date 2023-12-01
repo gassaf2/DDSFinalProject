97058b56bed4d7cbdcc602f56152dea293610c84
gassaf2 <gassaf2@gmail.com>
2023-12-01 09:22:36 +0400

    r

--- ROOFTOP/streamlit_test.py
+++ ROOFTOP/streamlit_test.py
@@ -19,12 +19,12 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+    #st.write(bytes_data)
 
     # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     st.write(stringio)
-
+	print("AAAAAAAAAAAAAAAAAAAAAAAAAAA")
     # To read file as string:
     string_data = stringio.read()
     st.write(string_data)
