fcdcee8cb9905af55183b98ea36ce49598bc914c
gassaf2 <gassaf2@gmail.com>
2023-12-01 08:48:29 +0400

    d

--- ROOFTOP/streamlit_test.py
+++ ROOFTOP/streamlit_test.py
@@ -15,12 +15,23 @@ code = '''def hello():
     print("Hello, Streamlit!")'''
 st.code(code, language='python')
 """
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
-# Using object notation
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone")
